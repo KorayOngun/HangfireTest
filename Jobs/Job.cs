8ddc66a99f8ba897dd4f95d187a4f56c779b8b79
koray öngün <koraygg28@gmail.com>
2023-05-22 18:19:50 +0300

    Proje dosyası ekle.

---
+++ Jobs/Job.cs
@@ -0,0 +1,10 @@
+﻿namespace HangFireDeneme.Jobs
+{
+    public class Job
+    {
+        public void HangfireControl()
+        {
+            Console.WriteLine("hello world from hangfire");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HangFireDeneme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
